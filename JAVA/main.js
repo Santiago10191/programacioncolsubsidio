dbc82d69c1f012da7f2c92baf0a6bcb4605e4add
cuellarsantiago6@gmail.com <cuellarsantiago6@gmail.com>
2023-04-03 08:01:50 -0500

    carga JAVA

---
+++ JAVA/main.js
@@ -0,0 +1,7 @@
+document.write("prueba")
+var nombre ="Santiago Cuellar"
+var datos = document.getElementById("datos")
+datos.innerHTML= `
+    <h1> pruebas java</h1>
+    <h3>Trabajo presentado por ${nombre}</h3>
+`
